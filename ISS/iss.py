e6fd2ffe0cbc1ce03d722143fdd32a352c9c6d7a
apreciado4 <80650676+apreciado4@users.noreply.github.com>
2022-01-15 04:01:12 -0800

    Update iss.py
    Added turtle and GPS

--- ISS/iss.py
+++ ISS/iss.py
@@ -3,15 +3,69 @@ This program downloads a json that returns a list of people currently in the
 International Space Station and prints
 """
 import json
-# import turtle
+import datetime
+import time
+import turtle
 import urllib.request
 
-url = "http://api.open-notify.org/astros.json"
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
+sflat = 37.710227966308594
+sflong= -122.43498992919922
 
-print("There are", result["number"], "people in space")
+image = 'map.gif'
 
-people = result["people"]
+peopleurl ="http://api.open-notify.org/astros.json"
+gpsurl = "http://api.open-notify.org/iss-now.json"
+passoverurl ="http://api.open-notify.org/iss-pass.json"
+passoverurl = passoverurl + '?lat=' + str(sflat) + '&lon=' + str(sflong)
+
+
+def getdata(url):
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    return result
+
+peopledata = getdata(peopleurl)
+print("There are", peopledata['number'], "people in space")
+people = peopledata['people']
 for p in people:
-    print(p["name"])
+    print('{name} is aboard the {craft}'.format(
+                name = p['name'],
+                craft =p['craft']))
+
+gpsdata = getdata(gpsurl)
+timestamp = datetime.datetime.fromtimestamp(gpsdata['timestamp'])
+position = gpsdata['iss_position']
+longitude = float(position['longitude'])
+latitude = float(position['latitude'])
+print('\nAt {}'.format(timestamp))
+print('The ISS was at latitude {lat}, and longitude {long}'.format(
+                lat = latitude,
+                long = longitude))
+
+passoverdata = getdata(passoverurl)
+passover = passoverdata['response'][1]['risetime']
+# print(time.ctime(passover))
+
+screen = turtle.Screen()
+screen.setup(720,360)
+screen.setworldcoordinates(-180,-90,180,90)
+screen.bgpic(image)
+
+location = turtle.Turtle()
+location.penup()
+location.color('orange')
+location.goto(sflong, sflat)
+location.dot(10)
+location.hideturtle()
+
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
+iss.penup()
+iss.goto(longitude, latitude)
+
+style = ('arial', 6, 'bold')
+location.write(time.ctime(passover), font='style')
+
+turtle.done()
